a4777de1c0e7a1b55d2372eec240dc4f7be450a1
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2025-04-10 00:20:36 +0200

    implement multiplier

--- ue04/1_zaehler/src/counter_ea_struct.vhd
+++ ue04/1_zaehler/src/counter_ea_struct.vhd
@@ -9,11 +9,18 @@ entity counter is
 end;    
 
 architecture struct of counter is
+
+ signal ffa_outputs: std_ulogic_vector(width -1);  
+  component ff  port(clk, reset:  in std_ulogic;
+       d:           in std_ulogic_vector(width-1 downto 0);
+       q:          out std_ulogic_vector(width-1 downto 0));
   
-  
-  
+  end component;
 begin
 
+ ff0: ff port map(clk, reset, up, ffa_outputs(width -1) );
+  ffN: for i in width - 2 downto 0 generate
 
+  end generate;
 
 end;
